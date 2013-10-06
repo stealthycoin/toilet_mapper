0fdf81f5ce2d1ffe4f7727811540b9edbae03ce6
stealthycoin <john.w.carlyle@gmail.com>
2013-10-06 14:14:15 -0700

    Installed the app I jsut made.

--- toilet_mapper/localsettings.py
+++ toilet_mapper/localsettings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'main',
 )
 
 # A sample logging configuration. The only tangible logging
