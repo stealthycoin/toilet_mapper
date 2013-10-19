6b334b9f403f9bd4f0f06e7cf5db59541939cd91
stealthycoin <john.w.carlyle@gmail.com>
2013-10-18 17:06:13 -0700

    Moved WSGI handler

--- toilet_mapper/wsgi.py
+++ toilet_mapper/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'toilet_mapper.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
