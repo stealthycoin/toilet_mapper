b3c91645284fba7d0c0698267c6884c9b425c43e
StealthyCoin <john.w.carlyle@gmail.com>
2013-11-30 15:14:13 -0800

    Model added for a Report

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from review.models import Review
 
 class AdditionalUserInfo(models.Model):
     user = models.ForeignKey(User)
@@ -11,3 +12,8 @@ class AdditionalUserInfo(models.Model):
 
     def __unicode__(self):
         return u"%s" % self.user
+
+
+class Report(models.Model):
+    user = models.ForeignKey(User)
+    review = models.ForeignKey(Review)
