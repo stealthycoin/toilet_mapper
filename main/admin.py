1485a025fb5fb764926f6979afe8c054250916a5
StealthyCoin <john.w.carlyle@gmail.com>
2013-11-30 15:04:23 -0800

    ability to sort users by spam count

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from models import AdditionalUserInfo
 
-admin.site.register(AdditionalUserInfo)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'spamCount')
+
+admin.site.register(AdditionalUserInfo, UserAdmin)
