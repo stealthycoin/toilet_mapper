9832757e5174a2051f428e674ff835333124ffaa
StealthyCoin <john.w.carlyle@gmail.com>
2013-11-30 15:14:40 -0800

    Show reports in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import AdditionalUserInfo
+from models import AdditionalUserInfo, Report
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('user', 'spamCount')
 
 admin.site.register(AdditionalUserInfo, UserAdmin)
+admin.site.register(Report)
